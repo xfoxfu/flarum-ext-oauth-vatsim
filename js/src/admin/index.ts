6890d153a13f75f0ac9e23ffdaad334326660a26
Yuze Fu <i@xfox.me>
2024-06-12 14:44:06 +0900

    chore: code cleanup

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,6 +1,6 @@
 import app from 'flarum/admin/app';
 import { ConfigureWithOAuthPage } from '@fof-oauth';
 
-app.initializers.add('ianm/oauth-amazon', () => {
-  app.extensionData.for('ianm-oauth-amazon').registerPage(ConfigureWithOAuthPage);
+app.initializers.add('xfoxfu/oauth-vatsim', () => {
+  app.extensionData.for('xfoxfu-oauth-vatsim').registerPage(ConfigureWithOAuthPage);
 });
